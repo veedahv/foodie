9149cf35e9612ff59319232beb3aa4ee451a45ed
veedahv <veedahv55@gmail.com>
2022-02-01 16:17:32 +0100

    validated form

--- src/app/module/contact/contact-page/contact-page.component.ts
+++ src/app/module/contact/contact-page/contact-page.component.ts
@@ -15,47 +15,27 @@ export class ContactPageComponent implements OnInit {
   constructor(private cuisine: CuisinesService, private formBuilder: FormBuilder,) { }
 
   post: any = []
-  checkoutForm: FormGroup = this.formBuilder.group({
+  postForm: FormGroup = this.formBuilder.group({
     userId: ['', Validators.required],
     title: '',
     body: ''
   });
-  // userId: new FormControl(RequiredValidator),
   
   ngOnInit() {
-    // this.sendPost();
-      // this.checkoutForm = this.formBuilder.group({
-      //   userId: ['', Validators.required],
-      //   title: '',
-      //   body: ''
-      // });
+    
   }
 
   sendPost() {
-    // let countrySelected = this.checkoutForm.get('userId').value;
-    // if(countrySelected === 'B') {
-    //   this.checkoutForm.get('county').setValidators([Validators.required]); // 5.Set Required Validator
-    //   this.checkoutForm.get('county').updateValueAndValidity();
-    // } else {
-    //   this.checkoutForm.get('county').clearValidators(); // 6. Clear All Validators
-    //   this.checkoutForm.get('county').updateValueAndValidity();
-    // }
-    if (this.checkoutForm.value.userId !== '' || this.checkoutForm.value.title !== '' || this.checkoutForm.value.body !== '') {
-      this.cuisine.create(JSON.stringify(this.checkoutForm.value)).subscribe(data => {
+    if (this.postForm.value.userId !== '' || this.postForm.value.title !== '' || this.postForm.value.body !== '') {
+      this.cuisine.create(JSON.stringify(this.postForm.value)).subscribe(data => {
         this.post = data;
         console.log(this.post);
       })      
+    alert(`Your form has been submitted`);
+    this.postForm.reset();
     } else {
       alert('Input field(s) can not be empty');
     }
-  }
-
-  onSubmit(): void {
-  this.sendPost();
-    // Process checkout data here
-    // this.items = this.cartService.clearCart();
-    console.warn('Your order has been submitted', this.checkoutForm.value);
-    this.checkoutForm.reset();
   }
 
 }
